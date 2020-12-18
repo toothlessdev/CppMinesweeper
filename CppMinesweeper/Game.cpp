2387f73948130b38be722a73e6d7a2621da08e11
repl.it user <replituser@example.com>
2020-12-18 13:15:39 +0000

    Modify main.cpp

--- CppMinesweeper/Game.cpp
+++ CppMinesweeper/Game.cpp
@@ -31,10 +31,10 @@ void Game::GetKeyboardInput(){
 	if(!isDisplayed[inputY][inputX]){ //확인 하지 않은 칸이면
 		int select;
 		std::cout << "선택된 영역을 어떻게 하시겠습니까?" << '\n';
-		std::cout << "1. 확인한다   2. 깃발을 꽃는다   3. 돌아간다<< '\n';
+		std::cout << "1. 확인한다   2. 깃발을 꽃는다   3. 돌아간다" << '\n';
 		std::cin >> select;
 
-		if(is)
+		
 	}
 	else{
 		std::cout << "이미 확인했던 칸입니다!" << '\n';
